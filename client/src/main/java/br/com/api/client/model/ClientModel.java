58708260367b9f4d4a09b6ff1e2fe129c7f39592
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-18 21:10:57 -0300

    Criando o nosso model e atribuindo os valores, com os metodos codigo, nome e marca, iniciando a construção da classe Cliente controller e do seus atributos e metodos

---
+++ client/src/main/java/br/com/api/client/model/ClientModel.java
@@ -0,0 +1,27 @@
+
+package br.com.api.client.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "client")
+public class ClientModel {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+    private String name;
+    private String marca;  
+
+}
+
+   
+    
+
