7c46d53c0d4b428d1d827eb06f45c576d2f4d8ff
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-19 15:39:13 -0300

    Update client service, criando os HTTP status code.

--- client/src/main/java/br/com/api/client/services/ClientService.java
+++ client/src/main/java/br/com/api/client/services/ClientService.java
@@ -1,41 +1,38 @@
 package br.com.api.client.services;
 
 import br.com.api.client.model.ClientModel;
+import br.com.api.client.model.ResModel;
 import br.com.api.client.repositorio.ClientRepositorio;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import lombok.Getter;
-import lombok.Setter;
 
 @Service
-@Getter
-@Setter
 public class ClientService {
 
     @Autowired
     private ClientRepositorio clientRepositorio;
     @Autowired
-    private ClientModel rModel;
+    private ResModel rm;
 
     public Iterable<ClientModel> listarTodos() {
         return clientRepositorio.findAll();
     }
-
-    public ResponseEntity<?> cadastraClient(ClientModel pm) {
-        if (pm.getName() == null || pm.getName().isEmpty()) {
-            pm.setName("Sem nome");
-            return new ResponseEntity<>(pm, HttpStatus.BAD_REQUEST);
-        } else if (pm.getMarca() == null || pm.getMarca().isEmpty()) {
-            pm.setMarca("Sem marca");
-            return new ResponseEntity<>(pm, HttpStatus.BAD_REQUEST);
-        } else {
-            return new ResponseEntity<>(clientRepositorio.save(pm), HttpStatus.CREATED);
-
+   
+    //metodo para salvar cliente
+    public ResponseEntity<?> cadastra(ClientModel md){
+        if(md.getName().equals("") || md.getName() == null){
+            rm.setMensagem("Nome não pode ser vazio");
+            return ResponseEntity.badRequest().body(rm);
+        }else if(md.getMarca().equals("") || md.getMarca() == null){
+            rm.setMensagem("Marca não pode ser vazio");
+            return ResponseEntity.badRequest().body(rm);
+        }else{
+            clientRepositorio.save(md);
+            rm.setMensagem("Cliente cadastrado com sucesso");
+            return ResponseEntity.ok(rm);
         }
-    }
-
+    
+    }    
 }
